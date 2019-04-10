99e5c5df45de2543dfd12a5a8a572a21fd559408
Alexandre Ruban <alexandre.ruban@gmail.com>
2019-04-10 20:03:20 +0200

    finalized API router shopping cart

---
+++ routing/shopping_cart/src/app/components/login/LoginBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="login" class="box has-text-centered">
+    <h2 class="title">Fullstack Clothing</h2>
+    <button
+      @click="login"
+      class="button is-primary"
+      :class="{ 'is-loading': loading }" >
+      Login
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "LoginBox",
+  methods: {
+    login() {
+      this.$store.dispatch("login").then(() => {
+        this.$router.push("/products");
+      })
+    }
+  },
+  computed: {
+    ...mapGetters(["loading"])
+  }
+}
+</script>
+
+<style scoped>
+.box {
+  padding: 30px;
+}
+</style>
